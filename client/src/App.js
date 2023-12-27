b7de018c682f4ec09c35d808803fce7943b697db
Sagar Gupta <sg85207@gmail.com>
2023-12-27 08:51:57 +0530

    Update project files

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,15 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 
 function App() {
-	const user = localStorage.getItem("token");
-	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-		</Routes>
-	);
+  const user = localStorage.getItem("token");
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
+  );
 }
 
 export default App;
--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
- app.use(express.static('client/build'));
- app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname + '/client/build/index.html'));
- });
-}
-
-
 // database connection
 connection();
 
@@ -27,3 +19,6 @@ app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
+
+// link static React build
+app.use(express.static("./client/build"));
